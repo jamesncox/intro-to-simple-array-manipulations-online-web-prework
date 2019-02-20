3fa9266dbb889f4f27d87d56431785da955bae7b
James Cox <jamesncox@yahoo.com>
2019-02-20 18:49:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,4 +26,5 @@ def using_concat(my_favorite_things, all_my_favs)
   my_favorite_things.concat(all_my_favs)
 end
 
-def using_insert(list_of_esoteric_programming_languages, )
+def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
+  list_of_esoteric_programming_languages.insert(another_esoteric_language)
