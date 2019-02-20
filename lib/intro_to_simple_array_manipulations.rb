c2a23890b9b40eeb200737fa4264463481995b1b
James Cox <jamesncox@yahoo.com>
2019-02-20 18:32:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,12 +10,12 @@ def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
 
-def using_pop(great_hits_of_the_nineties, deleted_string)
+def using_pop(great_hits_of_the_nineties)
    great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
    great_hits_of_the_nineties.pop
 end
  
- def pop_with_args(chars_in_game_of_thrones, chars_arya_killed)
+ def pop_with_args(chars_in_game_of_thrones)
    chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
    chars_in_game_of_thrones.pop(2)
 end
