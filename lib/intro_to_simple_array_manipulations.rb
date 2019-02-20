6275d7f6a8c77f269b6c1ebb663c7595c31da36c
James Cox <jamesncox@yahoo.com>
2019-02-20 18:04:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,5 +12,5 @@ end
 
 def using_pop
    great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-   great_hits_of_the_nineties.pop
+   great_hits_of_the_nineties.
  end
