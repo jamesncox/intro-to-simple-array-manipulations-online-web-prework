226c9fce9a01a6787d5c46c2a16b53131b50e6eb
James Cox <jamesncox@yahoo.com>
2019-02-20 18:23:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,7 +12,7 @@ end
 
 def using_pop(great_hits_of_the_nineties, deleted_string)
    great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-   deleted_string =  ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life"]
+   deleted_string = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life"]
    great_hits_of_the_nineties.pop
  end
  
