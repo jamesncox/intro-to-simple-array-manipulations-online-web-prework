f8c3af088f21e8a32ce1daaeb3d4aadf76e6f701
James Cox <jamesncox@yahoo.com>
2019-02-20 18:52:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,4 +30,6 @@ def using_insert(list_of_esoteric_programming_languages, another_esoteric_langua
   list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
 end
 
-def using_uniq()
+def using_uniq(captain_planet_and_the_planeteers)
+  captain_planet_and_the_planeteers.uniq
+end
